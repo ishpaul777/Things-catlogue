8f6dfee1e2eb0ca1e497241c95f536e4fb324346
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-19 21:06:42 +0530

    added schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,26 @@
+CREATE TABLE item (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  FOREIGN KEY (genre_id) REFERENCES genres (id),
+);
+
+CREATE TABLE book (
+  id  INT,
+  title VARCHAR(150),
+  publisher VARCHAR(150),
+  cover_state VARCHAR(150),
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE labels (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  title VARCHAR(100),
+  color VARCHAR(100),
+);
 
 CREATE TABLE music_album (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
