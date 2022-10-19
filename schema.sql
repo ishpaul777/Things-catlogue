d2f7da5b365802a0fbc842716b06191bbb8c6431
Esmatullah Ahamdzai <ea.ahmadzai2020@gmail.com>
2022-10-19 17:14:04 +0430

    add game and author classes with schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE music_album (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  on_spotify BOOLEAN,
+  publish_date DATE,
+  archived BOOLEAN
+);
+
+CREATE TABLE genre (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(200)
+);
+
+CREATE TABLE game (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  publish_date DATE,
+  archived BOOLEAN
+);
+
+CREATE TABLE author (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
