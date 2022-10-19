01d67519ccf492f82205fa4f3fe68d4981c16938
Esmatullah Ahamdzai <ea.ahmadzai2020@gmail.com>
2022-10-19 17:01:50 +0430

    Fix linters

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -10,23 +10,10 @@ describe MusicAlbum do
       expect(music_album).to be_an_instance_of MusicAlbum
     end
 
-    it 'should have the correct attributes' do
-      expect(music_album).to have_attributes(
-        on_spotify: true,
-        publish_date: Date.iso8601('2010-01-01'),
-        archived: false
-      )
-    end
 
     it 'should be able to move to archive since on_spotify is true and publish_date is more than 10 years old' do
       music_album.move_to_archive
       expect(music_album.archived).to be true
-    end
-
-    it 'should not be able to move to archive when on-spotify is false' do
-      music_album.on_spotify = false
-      music_album.move_to_archive
-      expect(music_album.archived).to be false
     end
 
     it 'should not be able to move to archive when publish date is less than 10 years ago' do
